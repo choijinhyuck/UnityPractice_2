b3e81b3cfc7fd9a33c140a97a8be3355422175a3
choijinhyuck <choijinhyuck@naver.com>
2023-05-09 17:02:02 +0900

    First commit for Shooting Game Project

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 플레이어가 이동할 속력
+    public float speed = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+        Vector3 dir = new Vector3(h, v, 0);
+        
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
